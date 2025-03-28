fde3429545b40c640a84be78c9471524d456b035
dan <contact@danmyers.net>
2025-03-28 12:43:10 -0500

    Added timeline

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,21 @@
-import { ContextProvider } from './components/Context'
+import { ContextProvider } from './components/Context';
 import Upload from './components/Upload';
+import Timeline from './components/Timeline';
 
 function App() {
   return (
     <ContextProvider>
-    <div className="bg-black h-screen
-      overflow-hidden w-screen flex
-      flex-col items-center"
-    >
-      {/* Upload Area */}
-      < Upload />
-      {/* Preview Area */}
-      {/* Timeline Area */}
-    </div>
+      <div className="
+        bg-black h-screen overflow-hidden
+        w-screen flex flex-col items-center
+        justify-between"
+      >
+        <Upload />
+        <Timeline />
+      </div>
     </ContextProvider>
   );
 }
 
-
 export default App;
+
---
+++ src/components/Timeline.tsx
@@ -0,0 +1,11 @@
+import {context} from "./Context";
+
+const Timeline = () => {
+
+  return (
+    <div className="h-40 w-screen bg-auto bg-gray-500">
+    </div>
+  );
+};
+
+export default Timeline;
