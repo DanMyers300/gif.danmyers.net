4e3d1a75cca747a90575238d97c6a89298ac281b
dan <contact@danmyers.net>
2025-03-28 16:19:00 -0500

    removed play/pause button

--- src/components/Preview.tsx
+++ src/components/Preview.tsx
@@ -1,5 +1,4 @@
 import React, { useCallback, useState, useRef } from "react";
-import { FaPlay, FaPause } from "react-icons/fa";
 import { useVideoContext } from "./Context";
 import ReactPlayer from "react-player";
 
@@ -19,29 +18,12 @@ const PreviewPlate: React.FC = () => {
           <>
             <ReactPlayer
               url={videoUrl}
-              controls={false}
-              className="
-                shadow border mx-auto
-                max-w-screen max-h-72
-                lg:max-h-120 md:max-h-100
-                border-bordercolor rounded"
+              width="80%"
               ref={playerRef}
               playing={playing}
               playsInline
+              onClick={handlePlayPause}
             />
-
-            <div className="flex flex-col items-center space-y-4">
-              <button
-                onClick={handlePlayPause}
-                className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4
-                         focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5
-                         mb-2 mt-4 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none
-                         dark:focus:ring-blue-800"
-                disabled={!videoUrl}
-              >
-                {playing ? <FaPause /> : <FaPlay />}
-              </button>
-            </div>
           </>
         )}
       </div>
