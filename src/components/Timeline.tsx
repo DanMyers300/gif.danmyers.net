eea442de8d879cb2f61e82b4adf6f04d82db3a7e
dan <contact@danmyers.net>
2025-03-28 18:35:36 -0500

    added greyed out area

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -1,4 +1,4 @@
-import {useState, useEffect} from "react";
+import { useState, useEffect } from "react";
 import { useVideoContext } from "./Context";
 import generateScreenshots from "../utils/generateScreenshots";
 
@@ -25,6 +25,16 @@ const Timeline = () => {
   return (
     <div className="absolute bottom-0 flex flex-col w-screen">
       <div className="timeline-container flex items-center justify-center bg-gray-700 min-h-40 max-h-40 overflow-hidden p-2 -space-x-6 relative">
+        {/* Left Overlay */}
+        <div
+          style={{ width: `${arrowPositions.left}%` }}
+          className="absolute top-0 left-0 h-full bg-gray-800 opacity-50"
+        />
+        {/* Right Overlay */}
+        <div
+          style={{ width: `${100 - arrowPositions.right}%`, left: `${arrowPositions.right}%` }}
+          className="absolute top-0 h-full bg-gray-800 opacity-50"
+        />
         {isGenerating ? (
           <div className="text-gray-400">Generating screenshots...</div>
         ) : screenshots.length > 0 ? (
