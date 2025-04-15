9af34424b369a2cab82bac3c9579b60a325b740f
dan <contact@danmyers.net>
2025-04-15 12:12:07 -0500

    moved arrow function

--- src/components/Arrows.tsx
+++ src/components/Arrows.tsx
@@ -1,56 +1,7 @@
 import { useVideoContext } from "./Context";
 import { useRef } from "react";
 import { FaArrowDown } from "react-icons/fa";
-
-const useArrowDrag = (
-  arrow: keyof ArrowPosition,
-  setArrowPositions: React.Dispatch<React.SetStateAction<ArrowPosition>>,
-  containerRef: React.RefObject<HTMLDivElement | null>,
-  onDrag: (arrow: keyof ArrowPosition, percent: number) => void
-) => {
-  const handleMove = (clientX: number) => {
-    if (!containerRef.current) return;
-
-    const containerRect = containerRef.current.getBoundingClientRect();
-    let newPosition =
-      ((clientX - containerRect.left) / containerRect.width) * 100;
-    newPosition = Math.max(0, Math.min(100, newPosition));
-
-    let clampedPosition = newPosition;
-    if (arrow === "left" && clampedPosition > 50) clampedPosition = 50;
-    if (arrow === "right" && clampedPosition < 50) clampedPosition = 50;
-
-    onDrag(arrow, clampedPosition);
-
-    setArrowPositions((prev) => ({
-      ...prev,
-      [arrow]: clampedPosition,
-    }));
-  };
-
-  const handleMouseMove = (e: MouseEvent) => handleMove(e.clientX);
-  const handleTouchMove = (e: TouchEvent) => handleMove(e.touches[0].clientX);
-
-  const handleMouseUp = () => {
-    window.removeEventListener("mousemove", handleMouseMove);
-    window.removeEventListener("mouseup", handleMouseUp);
-  };
-  const handleTouchEnd = () => {
-    window.removeEventListener("touchmove", handleTouchMove);
-    window.removeEventListener("touchend", handleTouchEnd);
-  };
-
-  const handleMouseDown = () => {
-    window.addEventListener("mousemove", handleMouseMove);
-    window.addEventListener("mouseup", handleMouseUp);
-  };
-  const handleTouchStart = () => {
-    window.addEventListener("touchmove", handleTouchMove);
-    window.addEventListener("touchend", handleTouchEnd);
-  };
-
-  return { handleMouseDown, handleTouchStart };
-};
+import useArrowDrag from "../utils/arrowDrag";
 
 type ArrowPosition = { left: number; right: number };
 
