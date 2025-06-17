83319a86e05fe611dae2b37ba01c107addb65bd2
dan <contact@danmyers.net>
2025-06-17 18:05:33 -0500

    added mobile check

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,23 @@
+import { isMobile } from "react-device-detect";
 import SideBar from "./components/SideBar";
 import EditorArea from "./components/EditorArea";
 
 const App = () => {
-
   return (
     <>
-      <main className="bg-[#322943] min-h-screen min-w-screen flex">
+      {isMobile ? (
+        <div className="bg-[#322943] min-h-screen min-w-screen flex items-center justify-center text-white text-2xl p-4 text-center">
+          <h1>Not Supported on Mobile</h1>
+        </div>
+      ) : (
+        <main className="bg-[#322943] min-h-screen min-w-screen flex">
+          <SideBar />
+          <EditorArea />
+        </main>
+      )}
 
-        < SideBar />
-        < EditorArea />
-
-      </main>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
