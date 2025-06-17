1bd20c4e461cc2ffd0ff81bc787244cfeaf9368b
dan <contact@danmyers.net>
2025-06-17 17:51:33 -0500

    added react-player

--- src/components/EditorArea.tsx
+++ src/components/EditorArea.tsx
@@ -3,11 +3,18 @@ import ReactPlayer from "react-player";
 const EditorArea = () => {
   return (
     <div className="flex flex-grow justify-center items-center">
-      <div className="bg-[#463a5e] w-19/20 min-h-19/20">
-        < ReactPlayer />
+      <div className="bg-[#463a5e] w-19/20 h-19/20 flex justify-center items-center">
+        <div className="flex justify-center items-center w-2/3 h-2/3 mb-50">
+          <ReactPlayer
+            url="https://youtu.be/9PldqVePztM"
+            width="100%"
+            height="100%"
+            controls={true}
+          />
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default EditorArea;
