9464329549c1ab20d13f84582857482d34835a20
dan <contact@danmyers.net>
2025-06-17 17:37:58 -0500

    added react-player

--- src/components/EditorArea.tsx
+++ src/components/EditorArea.tsx
@@ -1,7 +1,10 @@
+import ReactPlayer from "react-player";
+
 const EditorArea = () => {
   return (
     <div className="flex flex-grow justify-center items-center">
       <div className="bg-[#463a5e] w-19/20 min-h-19/20">
+        < ReactPlayer />
       </div>
     </div>
   )
